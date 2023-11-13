030127798ac33a8e52f1999312ea19c0f2bffcd8
mittiii <srp5628comust@gmail.com>
2023-11-14 01:40:30 +0900

    フォルダ構造を整理

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Start from '../Routes/Start/index';
+import Game from '../Routes/Game/index';
+import Result from '../Routes/Result/index';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* スタートページ */}
+        <Route path="/" element={<Start />} />
+
+        {/* ゲームページ */}
+        <Route path="/game" element={<Game />} />
+
+        {/* 結果ページ */}
+        <Route path="/result" element={<Result />} />
+
+        {/* 未定義のパスにアクセスした場合、スタートページへリダイレクト */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
