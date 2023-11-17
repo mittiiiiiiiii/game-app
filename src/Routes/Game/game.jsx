b29882989ab4618e553c8dbf4f49e62f385abca8
mittiii <srp5628comust@gmail.com>
2023-11-18 03:58:20 +0900

    Gameコンポーネントの基本的なレイアウトを作成&Startコンポーネントへ遷移する機能を実装&Gameコンポーネントテストの期待されたテキストで正しくレンダリングされるか確認するテストを実装&プレイボタンが存在するか確認するテストを実装

--- src/Routes/Game/game.jsx
+++ src/Routes/Game/game.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
+import { useNavigate } from 'react-router-dom'; 
 
 // コンポーネント定義
 const Game = () => {
+  const navigate = useNavigate();
+
+  //Startコンポーネントに遷移
+  const handlePlayButtonClick = () => {
+    navigate('/');
+  };
+
   return (
     <>
       <GlobalStyle />
@@ -10,7 +18,10 @@ const Game = () => {
           <Background/>
             <Header data-testid="header-label">NS-TYPING</Header>
             <BlackBoxContainer>
-              
+          <InstructionText>表示された数字または記号のキーを押してください</InstructionText>
+          <SymbolDisplay>@</SymbolDisplay>
+          <QuestionStats>問題数: 10<br/><br/><br/>正解数: 1</QuestionStats>
+          <ReturnButton onClick={handlePlayButtonClick}>タイトルに戻る</ReturnButton>
             </BlackBoxContainer>
         </Container>
     </>
@@ -74,4 +85,62 @@ const BlackBoxContainer = styled.div`
   transform: translateX(-50%);
   background: #1E1E1E;
   border: 10px solid yellow;
+`;
+
+const InstructionText = styled.div`
+  width: 435px;
+  height: 19px;
+  position: absolute;
+  left: 132px;
+  top: 70px;
+  text-align: center;
+  color: white;
+  font-size: 16px;
+  font-family: Arial;
+  font-weight: 400;
+  word-wrap: break-word;
+`;
+
+const ReturnButton = styled.div`
+  width: 138px;
+  height: 32px;
+  position: absolute;
+  left: 281px;
+  top: 412px;
+  background: #008000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 16px;
+  font-family: Arial;
+  font-weight: 400;
+  cursor: pointer; // ボタンとしての視覚的なフィードバック
+`;
+
+const SymbolDisplay = styled.div`
+  width: 154px;
+  height: 110px;
+  position: absolute;
+  left: 273px;
+  top: 187px;
+  text-align: center;
+  color: white;
+  font-size: 90px;
+  font-family: Arial;
+  font-weight: 400;
+  word-wrap: break-word;
+`;
+
+const QuestionStats = styled.div`
+  width: 91px;
+  height: 92px;
+  position: absolute;
+  left: 67px;
+  top: 352px;
+  color: white;
+  font-size: 16px;
+  font-family: Arial;
+  font-weight: 400;
+  word-wrap: break-word;
 `;
--- src/start.test.js
+++ src/start.test.js
@@ -46,12 +46,34 @@ describe('Game コンポーネント', () => {
   test('期待されたテキストで正しくレンダリングされる', () => {
     render(<Router><Game /></Router>);
 
-     // Header のテキストをテスト
+    // Header のテキストをテスト
     const headerLabel = screen.getByTestId('header-label');
     expect(headerLabel).toBeInTheDocument();
     expect(headerLabel).toBeVisible();
     expect(headerLabel).toHaveTextContent('NS-TYPING');
 
+    // InstructionText の存在と内容を確認
+    const instructionText = screen.getByText('表示された数字または記号のキーを押してください');
+    expect(instructionText).toBeInTheDocument();
+    expect(instructionText).toBeVisible();
+
+    // SymbolDisplay の存在と内容を確認
+    const symbolDisplay = screen.getByText('@');
+    expect(symbolDisplay).toBeInTheDocument();
+    expect(symbolDisplay).toBeVisible();
+
+    // QuestionStats の存在と内容を確認
+    const questionStats = screen.getByText(/問題数: 10/);
+    expect(questionStats).toBeInTheDocument();
+    expect(questionStats).toBeVisible();
+  });
+  test('リターンボタンが存在するか', () => {
+    render(<Router><Game /></Router>);
+
+    // プレイボタンのテキストをテスト
+    const playButton = screen.getByText('タイトルに戻る');
+    expect(playButton).toBeInTheDocument();
+    expect(playButton).toBeVisible();
   });
 });
 
