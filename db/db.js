4910446c422709c8943ab6e5bead0fde06ab7546
mittiii <srp5628comust@gmail.com>
2023-12-27 23:53:55 +0900

    dbを操作するAPIを作成した

--- db/db.js
+++ db/db.js
@@ -7,6 +7,11 @@ app.use(express.json());
 
 const sequelize = new Sequelize('postgres://postgres:postgresSuperUserPsw@mypostgres:5432/dbname');
 
+app.get('/results/last', async (req, res) => {
+    const lastResult = await Result.findOne({ order: [['createdAt', 'DESC']] });
+    res.json(lastResult);
+});
+
 const Result = sequelize.define('result', {
     elapsedTime: Sequelize.FLOAT,
     correctCount: Sequelize.INTEGER,
@@ -19,7 +24,7 @@ sequelize.sync().then(() => {
     console.log('Database & tables created!');
 });
 
-app.post('/results', async (req, res) => {
+app.post('/results/save', async (req, res) => {
     const result = await Result.create(req.body);
     res.json(result);
 });
--- src/Routes/Result/result.jsx
+++ src/Routes/Result/result.jsx
@@ -1,7 +1,13 @@
-import React ,{useEffect} from 'react';
+import React ,{useEffect,useState} from 'react';
 import styled from 'styled-components';
 import { useNavigate, useLocation } from 'react-router-dom'; 
 import { GlobalStyle,Container, Background, Header, BlackBoxContainer } from '../../utils/StyledComponents';
+import axios from 'axios';
+
+const saveResult = async (result) => {
+  const response = await axios.post('http://koske-game.nip.io/results/save', result);
+  return response.data;
+};
 
 const formatTime = (timeInSeconds) => {
   const minutes = Math.floor(timeInSeconds / 60);
@@ -19,13 +25,32 @@ const Result = ({ gameStarted }) => {
   const navigate = useNavigate();
   const location = useLocation();
   const { elapsedTime = 0, correctCount = 0, mistypeCount = 0, accuracy = 0, averageKeystrokes = 0 } = location.state;
+  const [lastResult, setLastResult] = useState(null);
+
+  useEffect(() => {
+    const fetchLastResult = async () => {
+      const response = await axios.get('http://koske-game.nip.io/results/last');
+      setLastResult(response.data);
+    };
+  
+    fetchLastResult();
+  }, []);
   
   //不正なアクセスを禁止する
   useEffect(() => {
     if (!gameStarted || !location.state) {
       navigate('/');
+    } else {
+      const result = {
+        elapsedTime,
+        correctCount,
+        mistypeCount,
+        accuracy,
+        averageKeystrokes
+      };
+      saveResult(result);
     }
-  }, [gameStarted, location.state, navigate]);
+  }, [gameStarted, location.state, navigate, elapsedTime, correctCount, mistypeCount, accuracy, averageKeystrokes]);
 
   const formattedElapsedTime = formatTime(elapsedTime);
   const formattedAccuracy = Number(accuracy).toFixed(2);
@@ -45,11 +70,11 @@ const Result = ({ gameStarted }) => {
             <BlackBoxContainer>
               <ResultTitle>結果</ResultTitle>
                 <TextInfo>
-                  ・経過時間: <GreenText>{formattedElapsedTime}<br/></GreenText>
-                  ・正しく打ったキーの数: <GreenText>{correctCount}<br/></GreenText>
-                  ・平均キータイプ数: <GreenText>{formattedAverageKeystrokes}</GreenText>回/秒<br/>
-                  ・ミスタイプ数: <GreenText>{mistypeCount}<br/></GreenText>
-                  ・正確率: <GreenText>{formattedAccuracy}</GreenText>%
+                  ・経過時間: <GreenText>{formattedElapsedTime} (前回: {lastResult ? formatTime(lastResult.elapsedTime) : '00:00:00'})<br/></GreenText>
+                  ・正しく打ったキーの数: <GreenText>{correctCount} (前回: {lastResult ? lastResult.correctCount : 0})<br/></GreenText>
+                  ・平均キータイプ数: <GreenText>{formattedAverageKeystrokes} (前回: {lastResult ? Number(lastResult.averageKeystrokes).toFixed(1) : '0.0'})回/秒<br/></GreenText>
+                  ・ミスタイプ数: <GreenText>{mistypeCount} (前回: {lastResult ? lastResult.mistypeCount : 0})<br/></GreenText>
+                  ・正確率: <GreenText>{formattedAccuracy} (前回: {lastResult ? Number(lastResult.accuracy).toFixed(2) : '0.00'})%</GreenText>%
                 </TextInfo>
               <ReturnButton onClick={handlePlayButtonClick}>タイトルに戻る</ReturnButton>
             </BlackBoxContainer>
