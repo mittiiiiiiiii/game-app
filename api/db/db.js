a992de1900db34ef19d01f06b8dd55bca02d4e63
mittiii <srp5628comust@gmail.com>
2023-12-27 23:13:53 +0900

    dbに保存するAPIを作成

---
+++ api/db/db.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const Sequelize = require('sequelize');
+
+const app = express();
+const port = 4000;
+app.use(express.json());
+
+const sequelize = new Sequelize('postgres://postgres:postgresSuperUserPsw@mypostgres:5432/dbname');
+
+const Result = sequelize.define('result', {
+    elapsedTime: Sequelize.FLOAT,
+    correctCount: Sequelize.INTEGER,
+    averageKeystrokes: Sequelize.FLOAT,
+    mistypeCount: Sequelize.INTEGER,
+    accuracy: Sequelize.FLOAT
+});
+
+sequelize.sync().then(() => {
+    console.log('Database & tables created!');
+});
+
+app.post('/results', async (req, res) => {
+    const result = await Result.create(req.body);
+    res.json(result);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
